feec83a7695dc90990056a334a98b55d27e0227f
Gabriel Delépine <Yappli@users.noreply.github.com>
2014-02-12 18:51:05 +0100

    Fix a warning in chrome

--- scrolled.js
+++ scrolled.js
@@ -2,7 +2,7 @@
  * - scrolled.js -
  * Licence MIT
  * Written by Gabriel Delépine
- * Current version  0.1 (2014-02-12)
+ * Current version  0.2 (2014-02-12)
  * Requirement : No one, it is a framework-free fonction (ie : You do not need to include any other file in your page such as jQuery)
  * fork me on github : https://github.com/Yappli/scrolled.js (and don't forget to pull request !)
  * */
@@ -15,7 +15,7 @@
         var body = document.getElementsByTagName('body')[0],
             indexOfScrolledClass = body.className.indexOf('scrolled');
         
-        if(parseInt(document.documentElement.scrollTop || document.body.scrollTop) > 0)
+        if(parseInt(document.documentElement.scrollTop !== undefined ? document.documentElement.scrollTop : document.body.scrollTop) > 0)
         {
             if(indexOfScrolledClass == -1)
                 body.className += (body.className.length == 0 ? '' : ' ')+'scrolled';
